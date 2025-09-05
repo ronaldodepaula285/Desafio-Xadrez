06f1769c9554683b6cb881049383c00cb12acc75
Ronaldo de Paula <ronaldodepaula1225@gmail.com>
2025-09-04 21:02:35 -0300

    atualização

--- desafioxadrez.c
+++ desafioxadrez.c
@@ -6,26 +6,26 @@ int main() {
     printf("Desafio Xadrez\n");
 
     // for usado para rmovimetar a torre cinco casas para cima
-    printf("Movimento da torre\n");
+    printf("\nMovimento da torre\n");
     for (int i = 0; i < 5; i++){
     printf("Uma casa para cima\n");
      }
     // while usado para mover a Rainha oito casas para esquerda
-    printf("Movimento da Rainha\n");
+    printf("\nMovimento da Rainha\n");
     int i = 1;
     while (i <= 8){
     printf("Uma casa para esquerda\n");
     i++;
     }
     // do while usado para mover o Bispo cinco casas na diagonal para cima/direita
-    printf("Movimento do Bispo\n");
+    printf("\nMovimento do Bispo\n");
     int j = 1;
     do {
     printf("Uma casa diagonal para cima/direita\n");
     j++;
     } while (j <= 5);
 // loop aninhado usado para mover o Cavalo três casas em L (duas para cima e uma para direita)
-    printf("Movimento do Cavalo\n");
+    printf("\nMovimento do Cavalo\n");
     int movimentocompleto = 1; // Variável de controle para o movimento completo do cavalo
     
     while (movimentocompleto--)
